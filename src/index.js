ef99b89b0ca557fc29e858272fc021b330ae0865
canofm <canofedericomartin@gmail.com>
2019-02-24 23:10:37 -0300

    Adding comparators to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import bunyan from "bunyan";
 import XPath from "./XPath";
 import DocumentReader from "./DocumentReader";
 import ComponentFinder from "./ComponentFinder";
+import ChildrenSizeComparator from "./comparators/ChildrenSizeComparator";
+import TextComparator from "./comparators/TextComparator";
+import TagComparator from "./comparators/TagComparator";
 
 const logger = bunyan.createLogger({ name: "component-finder" });
 
@@ -11,7 +14,11 @@ const diffPath = process.argv[3];
 const elementId = process.argv[4] || "make-everything-ok-button";
 
 const documentReader = new DocumentReader(logger);
-const componentFinder = new ComponentFinder({ logger });
+const componentFinder = new ComponentFinder({
+  logger,
+  adjuster: new ChildrenSizeComparator(),
+  comparators: [new TextComparator(0.2), new TagComparator(0.3)]
+});
 const xPath = new XPath();
 
 documentReader
