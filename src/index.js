8ea346c44ea32f3cf9f8468096cec837169a261e
canofm <canofedericomartin@gmail.com>
2019-02-24 22:29:28 -0300

    approach

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import bunyan from "bunyan";
+
+const logger = bunyan.createLogger({ name: "component-finder" });
+
+// data inputs
+const originalPath = process.argv[2];
+const diffPath = process.argv[3];
+const elementId = process.argv[4] || "make-everything-ok-button";
+
+const documentReader = new DocumentReader(logger);
+const componentFinder = new ComponentFinder(logger);
+const xPath = new XPath();
+
+documentReader
+  .getDoms(originalPath, diffPath)
+  .then(([originalHtml, diffHtml]) => componentFinder.find(elementId, originalHtml, diffHtml))
+  .then(element =>
+    logger.info(`Component path is: ${xPath.path(element)} and its html: ${element.innerHTML}`)
+  );
