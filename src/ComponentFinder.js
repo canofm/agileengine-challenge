a5a9dd8af2e51dcaec9b9ac1e04abc5b04188601
canofm <canofedericomartin@gmail.com>
2019-02-24 22:54:28 -0300

    Main approach for find element

--- src/ComponentFinder.js
+++ src/ComponentFinder.js
@@ -6,6 +6,7 @@ class ComponentFinder {
   }
 
   find(elementId, originalDom, diffDom) {
+    this.maxScore = 0;
     const originalElement = this.getElementById(elementId, originalDom);
 
     const elementFound = this._find(originalElement, diffDom);
@@ -16,7 +17,27 @@ class ComponentFinder {
     }
   }
 
-  _find(originalElement, dom) {}
+  _find(originalElement, dom) {
+    // Assume we're looking an element from body structure
+    const allComponents = Object.values(dom.window.document.body.children);
+    return this.adjuster
+      .adjustSample(originalElement, allComponents) // there is no need to look everything element, we will take just the ones which has the same amount of children
+      .reduce((maxElement, currentElement) => {
+        const score = this.compare(originalElement, currentElement);
+        if (score > this.maxScore) {
+          this.maxScore = score;
+          return currentElement;
+        }
+        return maxElement;
+      }, allComponents[0]);
+  }
+
+  compare(originalElement, otherElement) {
+    return this.comparators.reduce((accumScore, currentComparator) => {
+      const score = currentComparator.compare(originalElement, otherElement, this); // passing down the componentFinder because of probably recursion strategy to compare childs
+      return accumScore + score;
+    }, 0);
+  }
 
   getElementById(elementId, dom) {
     const element = dom.window.document.getElementById(elementId);
